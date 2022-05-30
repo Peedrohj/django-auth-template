6e47d49c8a37ffe43cd0e6b1d17bfb46c7038517
Pedro Farias <phjfg@cesar.org.br>
2022-05-30 02:37:56 +0000

    feat: :lock: Create validation for content_type in permission

--- code/core/domain/exceptions.py
+++ code/core/domain/exceptions.py
@@ -1,3 +1,7 @@
 class InvalidUUidException(Exception):
     def __init__(self, error='ID must be a valid UUID') -> None:  
-        super().__init__(error)  
+        super().__init__(error)
+
+class InvalidContentTypeException(Exception):
+    def __init__(self, error='Value must be a valid Content Type instance') -> None:  
+        super().__init__(error)
--- code/core/domain/permission.py
+++ code/core/domain/permission.py
@@ -4,9 +4,18 @@ from dataclasses import dataclass
 # Entities
 from core.domain.content_type import ContentType
 from core.domain.entity import BaseEntityId
+from core.domain.exceptions import InvalidContentTypeException
+
 
 @dataclass(kw_only=True, frozen=True, slots=True)
-class Permision(BaseEntityId):
+class Permission(BaseEntityId):
     name: str
     content_type: ContentType
     codename: str
+
+    def __post_init__(self):
+        self.__validate()
+
+    def __validate(self):
+        if not isinstance(self.content_type, ContentType):
+            raise InvalidContentTypeException()
