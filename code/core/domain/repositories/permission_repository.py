380e86b5e9cee2655231074300b9eca5bc46e89a
Pedro Farias <phjfg@cesar.org.br>
2022-06-02 01:01:21 +0000

    feat: Add abstract methods to permission repository

--- code/core/domain/repositories/permission_repository.py
+++ code/core/domain/repositories/permission_repository.py
@@ -1,20 +1,29 @@
 # Utils
-from abc import ABC
+from abc import ABC, abstractmethod
 from typing import Dict, List
 
 # Entities
 from core.domain.entity.permission import Permission
+from core.domain.value_objects import UniqueEntityId
 
 
 class PermissionRepository(ABC):
-    def find(self) -> Permission:
+    @abstractmethod
+    def insert(self, permission: Permission) -> Permission:
         raise NotImplementedError()
 
+    @abstractmethod
+    def find_by_id(self, permission_id: str | UniqueEntityId) -> Permission:
+        raise NotImplementedError()
+
+    @abstractmethod
     def find_all(self, filters: Dict = None) -> List[Permission]:
         raise NotImplementedError()
 
-    def save(self, permission: Permission) -> Permission:
+    @abstractmethod
+    def update(self, permission: Permission) -> Permission:
         raise NotImplementedError()
 
-    def delete(self, permission_id: str) -> None:
+    @abstractmethod
+    def delete(self, permission_id: str | UniqueEntityId) -> None:
         raise NotImplementedError()
